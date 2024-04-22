33de6f6cf5c6f2a144472f9f7300476b9994e24f
Zach Royer <zroyer@nerdwallet.com>
2024-04-22 11:29:12 -0700

    updates comment

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
+// Map of links to display in the side navigation!
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
